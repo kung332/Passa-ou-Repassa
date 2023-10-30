64d379fbbeb0919801d1c10fe7c2f5e7198ee038
otaviomd12 <125404697+otaviomd12@users.noreply.github.com>
2023-10-30 17:22:57 -0300

    Add files via upload

---
+++ jogo.js
@@ -0,0 +1,20 @@
+var score = 0;
+
+function checkAnswer(element, answerIndex) {
+  var correctAnswerIndex = 1; // Defina o índice da resposta correta
+
+  if (answerIndex === correctAnswerIndex) {
+    element.classList.add('correct');
+    score += 1; // Aumenta a pontuação em 1 ponto
+    document.querySelector('.score1').textContent = score;
+    document.querySelector('.score2').textContent = score;
+  } else {
+    element.style.display = 'none'; // Esconde a resposta errada
+  }
+
+  // Remova os eventos de clique após responder
+  var answers = document.getElementsByClassName('answer');
+  for (var i = 0; i < answers.length; i++) {
+    answers[i].removeAttribute('onclick');
+  }
+}
